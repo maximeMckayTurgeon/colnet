d0f11129c0ce76df675ba6553a2492c9a9d24231
Maxime McKay-Turgeon <maxmkt91@gmail.com>
2022-07-11 12:33:45 -0400

    clearDB pdo

--- connexion.php
+++ connexion.php
@@ -1,6 +1,14 @@
 
 <?php
-$servname = "localhost"; $user = "root"; $pass = ""; $dbname = "colnet";
-$connect = new PDO("mysql:host=$servname;dbname=$dbname;port=3306", $user, $pass);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+
+$connect = new PDO("mysql:host=$cleardb_server;dbname=$cleardb_db", $cleardb_username, $cleardb_password);
 $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  ?> 
